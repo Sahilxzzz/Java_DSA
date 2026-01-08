d17f86cee68729fe221f4c5a7eb4cc6d6176f3e1
Sahil Khude <shlkhude@gmail.com>
2026-01-08 21:58:24 +0530

    55th commit

--- arrays/set_Zero_Matrix.java
+++ arrays/set_Zero_Matrix.java
@@ -8,36 +8,24 @@ public class set_Zero_Matrix {
         int n = matrix.length;
         int m = matrix[0].length;
 
+        int [] row = new int[n];
+        int [] col = new int[m];
         for(i=0;i<n;i++){
             for(j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    markRow(matrix,i,n,m);
-                    markCol(matrix,j,n,m);
+                   row[i] = 1;
+                   col[j] = 1;
                 }
             }
         }
         for(i=0;i<n;i++){
             for(j=0;j<m;j++){
-                if(matrix[i][j]==-1){
-                   matrix[i][j] = 0;
+                if (row[i] == 1 || col[j]==1) {
+                    matrix[i][j] = 0;
                 }
             }
         }
 
     }
-    private void markRow(int[][]matrix,int i,int n, int m){
-        for(int j =0;j<m;j++){
-            if(matrix[i][j]!=0){
-                matrix[i][j] = -1;
-            }
-        }
-    }
-    private void markCol(int [][]matrix ,int j,int n, int m){
-        for(int i=0;i<n;i++){
-            if(matrix[i][j]!=0){
-                matrix[i][j] = -1;
-            }
-        }
-    }
-
 }
+    
