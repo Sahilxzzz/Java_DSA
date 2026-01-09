a951393b729dc417a3a7d3f08932827071f5d519
Sahil Khude <shlkhude@gmail.com>
2026-01-09 22:51:07 +0530

    56th commit

--- arrays/set_Zero_Matrix.java
+++ arrays/set_Zero_Matrix.java
@@ -8,24 +8,54 @@ public class set_Zero_Matrix {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int [] row = new int[n];
-        int [] col = new int[m];
-        for(i=0;i<n;i++){
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        //Checking for zeros in firstRow
             for(j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                   row[i] = 1;
-                   col[j] = 1;
+                if(matrix[0][j]==0){
+                   firstRowZero = true;
+                   break;
+                }
+            }
+        //Checking for zeros in firstCol
+            for(i=0;i<n;i++){
+                if(matrix[i][0]==0){
+                   firstColZero = true;
+                   break;
+                }
+            }
+
+        // Marking in First row and col from checking the zeros in the matrix
+        for(i=1;i<n;i++){
+            for(j=1;j<m;j++){
+                if (matrix[i][j]==0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-        for(i=0;i<n;i++){
-            for(j=0;j<m;j++){
-                if (row[i] == 1 || col[j]==1) {
+
+        //making the rows and col zero according to the marker
+         for ( i = 1; i < n; i++) {
+            for (j = 1; j < m; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
+        if(firstRowZero){
+            for(j=0;j<m;j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(firstColZero){
+            for(i=0;i<n;i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
+
 }
     
