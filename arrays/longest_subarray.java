71aaa9a395d0e76f48662865dd2d79c98c9ad049
Sahil Khude <shlkhude@gmail.com>
2026-01-21 20:28:31 +0530

    61st commit

--- arrays/longest_subarray.java
+++ arrays/longest_subarray.java
@@ -1,28 +1,43 @@
 package arrays;
 
+import java.util.*;
 class longestSubarray {
-    public int longestSubarray(int[] arr, int k) {
-        //int n = arr.length;
-        if (arr.length == 0) return 0;
 
-        int i = 0, sum = 0, maxLength = 0;
-
-        for (int j= 0; j < arr.length; j++) {
-
-            sum += arr[j];  // expand window
-
-            // shrink window if sum > k
-            while (sum > k) {
-                sum -= arr[i];
-                i++;
+    static int longestSubarray(int[]a,int n,int k){
+        int maxLen = 0;
+        int sum = 0;
+        
+        Map<Integer, Integer> presumMap = new HashMap<>();
+        
+        for(int i = 0; i<n;i++){
+            sum += a[i];
+            
+            if(sum == k){
+                maxLen = i+1;
             }
-
-            // check if sum == k
-            if (sum == k) {
-                maxLength = Math.max(maxLength, j - i + 1);
+            
+            int rem = sum - k ; 
+            if(presumMap.containsKey(rem)){
+                int len = i - presumMap.get(rem);
+                maxLen = Math.max(maxLen,len);
             }
+            if(!presumMap.containsKey(sum)){
+                presumMap.put(sum,i);
+            }
+            
         }
-
-        return maxLength;
+        return maxLen;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+    int [] a = {1,2,3,1,1,1,4,2,3};
+    int n = a.length;
+    int k = 3;
+    
+    int len = longestSubarray(a,n,k);
+        
+        System.out.println(len);
     }
 }
