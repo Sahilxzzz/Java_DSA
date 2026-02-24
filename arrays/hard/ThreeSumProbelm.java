4fdc17b4a54a0cf4a072b25d6a387dd242e5dc21
Sahil Khude <shlkhude@gmail.com>
2026-02-24 12:18:02 +0530

    70th commit - Optimal App for 3sum

---
+++ arrays/hard/ThreeSumProbelm.java
@@ -0,0 +1,79 @@
+// import java.util.List;
+// import java.util.*;
+
+// class ThreeSumProblem {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         Set<List<Integer>> st = new HashSet<>();
+
+//         int n = nums.length;
+
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 for(int k=j+1;k<n;k++){
+//                     if(nums[i]+nums[j]+nums[k] == 0){
+//                         List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+//                         Collections.sort(temp);
+//                         st.add(temp);
+//                     }
+//                 }
+//             }
+//         }
+//         return new ArrayList<>(st);
+//     }
+// }
+
+import java.util.*;
+
+// Class to solve 3-sum problem
+class Solution {
+    // Function to find triplets with sum zero
+    public List<List<Integer>> threeSum(int[] arr, int n) {
+        // Sort the array
+        Arrays.sort(arr);
+        // Store final result
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // First loop for first element
+        for (int i = 0; i < n; i++) {
+            // Skip duplicates for first element
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            // Two pointers
+            int left = i + 1, right = n - 1;
+
+            // Find pairs for current arr[i]
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(arr[i], arr[left], arr[right]));
+                    left++;
+                    right--;
+
+                    // Skip duplicates for left
+                    while (left < right && arr[left] == arr[left - 1]) left++;
+                    // Skip duplicates for right
+                    while (left < right && arr[right] == arr[right + 1]) right--;
+                }
+                else if (sum < 0) left++;
+                else right--;
+            }
+        }
+        return ans;
+    }
+}
+
+// Driver class
+public class ThreeSumProbelm {
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        int n = arr.length;
+        Solution obj = new Solution();
+        List<List<Integer>> res = obj.threeSum(arr, n);
+
+        for (List<Integer> triplet : res) {
+            for (int num : triplet) System.out.print(num + " ");
+            System.out.println();
+        }
+    }
+}
