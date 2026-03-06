3811b58c40d8dece5d5cbf14419169d8ee8dc902
Sahilxzzz <shlkhude@gmail.com>
2026-03-06 21:27:10 +0530

    Count of Subarray with xor K

--- arrays/Array2D.java
+++ arrays/Array2D.java
@@ -3,7 +3,7 @@ package arrays;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class array2d {
+public class Array2d {
    public static void main(String[] args) {
     Scanner sc = new Scanner (System.in);
 
--- arrays/hard/count_subarray_xor_k.java
+++ arrays/hard/count_subarray_xor_k.java
@@ -10,7 +10,7 @@ public class count_subarray_xor_k {
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 int xorVal = 0;
-                for(int k = i;k<j;k++){
+                for(int k = i;k<=j;k++){
                     xorVal ^= arr[k]; 
                    
                 }
@@ -22,6 +22,7 @@ public class count_subarray_xor_k {
         }
 
         return count;
+
     }
     public static void main(String[] args) {
         // Input array
