b8e39846b3656e2e155a229bc379990b548c3d31
Sahil Khude <shlkhude@gmail.com>
2026-04-11 21:01:05 +0530

    91st comit

---
+++ binary_search/floor_ceit.java
@@ -0,0 +1,56 @@
+package binary_search;
+
+import java.util.*;
+public class floor_ceit {
+    // Function to find floor
+    public int findFloor(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x) {
+                ans = arr[mid];     // Potential floor
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    // Function to find ceiling
+    public int findCeil(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = arr[mid];     // Potential ceil
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    // Function to return floor and ceil as array
+    public int[] getFloorAndCeil(int[] arr, int x) {
+        int f = findFloor(arr, x);
+        int c = findCeil(arr, x);
+        return new int[]{f, c};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 4, 7, 8, 10};
+        int x = 5;
+        floor_ceit finder = new floor_ceit();
+
+        int[] res = finder.getFloorAndCeil(arr, x);
+        System.out.println("The floor and ceil are: " + res[0] + " " + res[1]);
+    }
+}
+
+
